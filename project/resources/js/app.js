a8b545ec9d9e3b31a5d016214e37d5de4a314ff7
Lucas Vinícius <lvinicius123@gmail.com>
2024-03-13 10:46:00 -0300

    Quasar in Project

--- project/resources/js/app.js
+++ project/resources/js/app.js
@@ -1,1 +1,30 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3',
+import { Quasar} from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(Quasar, {
+        plugins:{},
+      })
+      .mount(el)
+  },
+})
